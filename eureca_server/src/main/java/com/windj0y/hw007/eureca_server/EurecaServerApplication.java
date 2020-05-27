c2c2a58e0e20b07448e68dd2ac2e7f918e171cdc
wj <398636806@qq.com>
2020-05-27 23:54:01 +0800

    server

---
+++ eureca_server/src/main/java/com/windj0y/hw007/eureca_server/EurecaServerApplication.java
@@ -0,0 +1,15 @@
+package com.windj0y.hw007.eureca_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurecaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurecaServerApplication.class, args);
+    }
+
+}
